3d135bfc93e802320e6a4535ac32b1fa9254da71
Sitthiphon17 <sitthiphon17112550@gmail.com>
2025-03-28 20:45:31 +0700

    Delete profile.js

--- profile.js
+++
@@ -1,5 +0,0 @@
-if (password === confirmPassword) {
-    alert("เข้าสู่ระบบสำเร็จ");
-    localStorage.setItem("user", emailOrPhone); // บันทึกข้อมูลผู้ใช้
-    window.location.href = "profile.html";
-}
